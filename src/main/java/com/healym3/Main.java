95c1849a2dc4779d0cf06f255bcf2a68e71c6280
healym3 <healym3@gator.uhd.edu>
2022-03-10 00:04:37 -0600

    Substitution breaker is now functional;

--- src/main/java/com/healym3/Main.java
+++ src/main/java/com/healym3/Main.java
@@ -18,5 +18,23 @@ public class Main {
         System.out.println(cipher);
         System.out.println(plain);
         Breaker breaker = new Breaker();
+        breaker.breakCipher("msr crkqimurem xz jxukdmri bjvrejr qec renverriven mrjsextxna vb xer " +
+                "xz msirr qjqcruvj crkqimuremb pvmsve msr jxttrnr xz bjvrejrb qec " +
+                "mrjsextxna qm msr devoribvma xz sxdbmxe cxpemxpe. pr xzzri bmdcremb q " +
+                "devwdr xkkximdevma mx qjsvror qjqcruvj bdjjrbb ve jxukdmri bjvrejr qec " +
+                "renverriven mrjsextxna msixdns ivnxixdb qec ryjvmven jxdibrb, " +
+                "vejtdcven decriniqcdqmr irbrqijs xkkximdevmvrb. pr qir jxuuvmmrc mx " +
+                "miqveven bmdcremb zxi jsqttrenrb ve jxukdmri bjvrejr qec renverriven " +
+                "mrjsextxna, qec ve zvrtcb irwdviven msr gexptrcnr qec bgvttb, xdi " +
+                "jdiivjdtdu kixovcrb zxi bmdcremb pvmsve mryqb qec qixdec msr pxitc. " +
+                "msr crkqimurem xz jxukdmri bjvrejrb qec renverriven mrjsextxna vb msr " +
+                "zqbmrbm nixpven crkqimurem pvmsve msr jxttrnr xz bjvrejrb qec " +
+                "mrjsextxna. qccvmvxeqtta, qb q bmdcrem xz msr jbrm crkqimurem, axd " +
+                "pvtt nqve sqecb xe rykrivrejr qec kiqjmvjqt miqveven. bmdcremb pvtt fr " +
+                "irwdvirc mx kqimvjvkqmr ve q brurbmri kixhrjm, psvjs pvtt miqve " +
+                "bmdcremb mx pxig xe irqt vecdbmia kixftrub ve q mrqu reovixeurem msqm " +
+                "msra pvtt rejxdemri pxigven ve vecdbmia qzmri niqcdqmvxe. msr brevxi " +
+                "kixhrjm qttxpb bmdcremb mx qkkta msrvi jxdibr uqmrivqtb qec trqieven " +
+                "rykrivrejrb mx brurbmri txen irqt pxitc kixhrjmb.");
     }
 }
--- src/main/java/com/healym3/breaker/Breaker.java
+++ src/main/java/com/healym3/breaker/Breaker.java
@@ -1,24 +1,23 @@
 package com.healym3.breaker;
 
 import java.io.*;
-import java.text.ParseException;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.*;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import java.io.InputStream;
-
-
 
 public class Breaker {
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     private static final int ALPHABET_SIZE = 26;
+    private static final int ROUNDS = 10000;
+    private static final int CONSOLIDATE = 3;
     private Map<Character, Integer> transCharToInt;
-    private int[] quadgramsArray;
-    // JsonObject quadgrams;
+    private Map<Integer, Character> transIntToChar;
+    private int[] quadgrams;
+
 
     public Breaker() {
 
@@ -27,32 +26,23 @@ public class Breaker {
         for (int i = 0; i < ALPHABET_SIZE; i++) {
             transCharToInt.put(ALPHABET.charAt(i), i);
         }
+        this.transIntToChar = new HashMap<Integer, Character>();
 
-//        String fileName = "src/main/java/com/healym3/breaker/EN.json";
-//        InputStream is = Breaker.class.getResourceAsStream(fileName);
-//        if (is == null) {
-//            throw new NullPointerException("Cannot find resource file " + fileName);
-//        }
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            transIntToChar.put(i, ALPHABET.charAt(i));
+        }
+
         try (FileReader reader = new FileReader("src/main/resources/EN.json"))
         {
             JSONTokener tokener = new JSONTokener(reader);
             JSONObject object = new JSONObject(tokener);
             System.out.println("alphabet " + object.getString("alphabet"));
             JSONArray quadgramsJson = object.getJSONArray("quadgrams");
-            this.quadgramsArray = new int[quadgramsJson.length()];
+            this.quadgrams = new int[quadgramsJson.length()];
             for (int i = 0; i < quadgramsJson.length(); i++) {
-                quadgramsArray[i] = quadgramsJson.optInt(i);
+                quadgrams[i] = quadgramsJson.optInt(i);
             }
 
-
-//            //Read JSON file
-//            Object obj = jsonParser.parse(reader);
-//
-//            JSONArray employeeList = (JSONArray) obj;
-//            System.out.println(employeeList);
-//
-//            //Iterate over employee array
-//            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -60,5 +50,124 @@ public class Breaker {
             e.printStackTrace();
         }
 
+    }
+
+    public void breakCipher(String cipher){
+        cipher = cipher.toLowerCase(Locale.ROOT);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char ch: cipher.toCharArray()
+             ) {
+            if(transCharToInt.containsKey(ch)){
+                stringBuilder.append(ch);
+            }
+        }
+        cipher = stringBuilder.toString();
+
+        int[] cipherBinary = new int[cipher.length()];
+        for (int i = 0; i < cipher.length(); i++) {
+            int ch = transCharToInt.get(cipher.charAt(i));
+            cipherBinary[i] = ch;
+
+        }
+        ArrayList<ArrayList<Integer>> charPositions = new ArrayList(ALPHABET_SIZE);
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            charPositions.add(new ArrayList<Integer>());
+        }
+
+        for (int i = 0; i < cipherBinary.length; i++) {
+            charPositions.get(cipherBinary[i]).add(i);
+        }
+
+        System.out.println(charPositions);
+
+        ArrayList<Integer> key = new ArrayList(ALPHABET_SIZE);
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            key.add(i);
+        }
+        ArrayList<Integer> bestKey = new ArrayList<>(ALPHABET_SIZE);
+        bestKey = (ArrayList<Integer>) key.clone();
+        int localMaximum = 0;
+        int localMaximumHit = 1;
+
+        for (int i = 0; i < ROUNDS; i++) {
+            Collections.shuffle(key, new SecureRandom());
+            int fitness = hillClimb(key, cipherBinary, charPositions);
+            System.out.println("fitness: " + fitness);
+            if (fitness > localMaximum){
+                localMaximum = fitness;
+                localMaximumHit = 1;
+                bestKey = (ArrayList<Integer>) key.clone();
+            } else if (fitness == localMaximum){
+                localMaximumHit +=1;
+                if (localMaximumHit == CONSOLIDATE){
+                    break;
+                }
+            }
+        }
+        System.out.println("Final: " + bestKey);
+        int textLength = cipherBinary.length;
+        int[] plaintext = new int[textLength];
+        for (int i = 0; i < textLength; i++) {
+            plaintext[i] = bestKey.indexOf(cipherBinary[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i: plaintext
+             ) {
+            sb.append(transIntToChar.get(i));
+        }
+        System.out.println(sb.toString());
+    }
+    private int hillClimb(ArrayList<Integer> key, int[] cipherBinary, ArrayList<ArrayList<Integer>> charPositions){
+        int textLength = cipherBinary.length;
+        int[] plaintext = new int[textLength];
+        for (int i = 0; i < textLength; i++) {
+            plaintext[i] = key.indexOf(cipherBinary[i]);
+        }
+
+        int maxFitness = 0;
+        boolean betterKey = true;
+        while(betterKey){
+            betterKey = false;
+            for (int idx1 = 0; idx1 < ALPHABET_SIZE-1; idx1++) {
+                for (int idx2 = idx1 + 1; idx2 < ALPHABET_SIZE; idx2++) {
+                    int ch1 = key.get(idx1);
+                    int ch2 = key.get(idx2);
+                    for (int index: charPositions.get(ch1)
+                         ) {
+                        plaintext[index] = idx2;
+
+                    }
+                    for (int index: charPositions.get(ch2)
+                         ) {
+                        plaintext[index] = idx1;
+                    }
+                    int tmpFitness = 0;
+                    int quad_idx = (plaintext[0] << 10) + (plaintext[1] << 5) + plaintext[2];
+                    for(int i = 3; i < textLength; i++){
+                        quad_idx = ((quad_idx & 0x7FFF) << 5) + plaintext[i];
+                        tmpFitness += quadgrams[quad_idx];
+                    }
+                    if(tmpFitness > maxFitness){
+                        maxFitness = tmpFitness;
+                        betterKey = true;
+                        key.set(idx1, ch2);
+                        key.set(idx2, ch1);
+                    } else {
+                        for (int index: charPositions.get(ch1)
+                        ) {
+                            plaintext[index] = idx1;
+
+                        }
+                        for (int index: charPositions.get(ch2)
+                        ) {
+                            plaintext[index] = idx2;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return maxFitness;
     }
 }
